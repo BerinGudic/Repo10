d5bb0d4ed2535f605d2b9dd5fbec11f08e163a79
BerinGudic <berin.gudic@gmail.com>
2022-04-06 15:04:45 +0200

    Added index.js file

---
+++ ex10/src/index.js
@@ -0,0 +1,30 @@
+function myFunction() {
+    var myMusic = {
+        001: {
+            artist: "Billy Joel",
+            title: "Piano Man",
+            release_year: 1973,
+            formats: {
+                1: "CD",
+                2: "8T",
+                3: "LP"
+            },
+            gold: true
+        },
+        002: {
+            id: 2,
+            artist: "Metallica",
+            title: "Prince Charming",
+            release_year: 1997,
+            formats: {
+                1: "CD",
+                2: "Vinyl",
+                3: "Record"
+            },
+        
+        }
+    };
+    return myMusic;
+}
+console.log(myFunction()[2]);
+module.exports = myFunction;
